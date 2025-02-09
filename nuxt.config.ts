4d743504a948ea32546e3a4eac171d663686a5ff
David Sabine <david.sabine@live.com>
2025-02-09 16:19:53 -0700

    biome linted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,66 +1,63 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
+	app: {
+		head: {
+			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+		},
+	},
+	colorMode: {
+		disableTransition: true,
+		preference: 'light',
+	},
+	compatibilityDate: '2024-07-11',
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    '@vueuse/nuxt',
-    'nuxt-og-image'
-  ],
+	devtools: {
+		enabled: false,
+	},
+	eslint: {
+		config: {
+			stylistic: {
+				commaDangle: 'never',
+				braceStyle: '1tbs',
+			},
+		},
+	},
+	extends: ['@nuxt/ui-pro'],
+	future: {
+		compatibilityVersion: 4,
+	},
+	hooks: {
+		// Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+		'components:extend': (components) => {
+			const globals = components.filter((c) =>
+				['UButton'].includes(c.pascalName),
+			)
 
-  devtools: {
-    enabled: true
-  },
+			globals.forEach((c) => (c.global = true))
+		},
+	},
+	nitro: {
+		prerender: {
+			routes: ['/', '/docs'],
+			crawlLinks: true,
+		},
+	},
+	modules: [
+		'@nuxt/content',
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/image',
+		'@nuxt/ui',
+		'@nuxthq/studio',
+		'@vueuse/nuxt',
+		'nuxt-og-image',
+	],
 
-  colorMode: {
-    disableTransition: true
-  },
-
-  routeRules: {
-    '/api/search.json': { prerender: true },
-    '/docs': { redirect: '/docs/getting-started', prerender: false }
-  },
-
-  future: {
-    compatibilityVersion: 4
-  },
-
-  compatibilityDate: '2024-07-11',
-
-  nitro: {
-    prerender: {
-      routes: [
-        '/',
-        '/docs'
-      ],
-      crawlLinks: true
-    }
-  },
-
-  typescript: {
-    strict: false
-  },
-
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
-
-      globals.forEach(c => c.global = true)
-    }
-  },
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
+	routeRules: {
+		'/api/search.json': { prerender: true },
+		'/docs': { redirect: '/docs/getting-started', prerender: false },
+	},
+	typescript: {
+		strict: false,
+	},
 })
---
+++ todo.md
@@ -0,0 +1,17 @@
+# To do
+
+1. biome configured
+1. prettier configured
+1. Logo
+1. Receipt download following purchase
+1. countOfCustomers calculates payment amount
+1. payment form
+1. Maillist for updates?
+1. FAQ pages?
+1. Video & blog articles?
+
+# Done
+
+1. favicon = 'logo'
+1. Brand colour at helcim
+1. invoice designer at helcim: https://frontiergunclub.myhelcim.com/invoices/theme-designer
